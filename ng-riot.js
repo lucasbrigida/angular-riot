0320a5edab4d9674901a3b3ea545df3f744d7144
lucasbrigida <grillobright@gmail.com>
2015-01-28 16:20:31 -0200

    :rocket: 1st

---
+++ ng-riot.js
@@ -0,0 +1,19 @@
+/*
+    ng-riot
+    @author: lucasbrigida <lucasbrigida@gmail.com>
+    @description: riot module for angular
+ */
+
+angular.module('ngRiot', [])
+  .factory('riot', ['$window', '$log',
+    function ($window, $log) {
+
+      //Check dependency
+      if (!$window.riot) {
+        return $log.error('Riot not installed, install package run: "bower install riot -g", More information: https://muut.com/riotjs/download.html');
+      }
+
+      return {
+
+      };
+}])
